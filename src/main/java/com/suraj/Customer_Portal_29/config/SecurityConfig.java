4f26d288e9e732d3d652f626d55605b6c986670c
Shantanu5787 <shantanu_gk.championxi@outlook.com>
2026-03-25 15:30:59 +0530

    Updated crossorigin added frontend URL

--- src/main/java/com/suraj/Customer_Portal_29/config/SecurityConfig.java
+++ src/main/java/com/suraj/Customer_Portal_29/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://solar-rts.onrender.com"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
